4468e7ff27c3c769efcff8d909ea648f4ca6d4e0
theSoberSobber <akshatshrivastava0010@gmail.com>
2022-10-18 18:25:53 +0530

    added mongoDb IP support and refactoring

--- features/ipHandler/ipHandler.js
+++ features/ipHandler/ipHandler.js
@@ -6,19 +6,8 @@ module.exports = async (ws, path_ipFile) => {
     const toTest = await readFile(path_ipFile, "utf-8")
 
     if(!(toTest===gotTest)){
-        const newUrl = `http://${gotTest}:3000/groupIds/`;
-        const res = await fetch(newUrl);
-        let obj = await res.text();
-        obj = await JSON.parse(obj);
-        for (const collegeName in obj){
-            for(const jid of obj[collegeName]){
-                console.log(jid);
-                const message = `the IP for the server has changed, we're sorry for the inconvineance caused.
-The new URL for your college is ${gotTest}:3000/${collegeName}/group`
-                await ws.sendMessage(jid, { text: message });
-                await ws.sendMessage(jid, { text: `Brought to you by https://alert-bot.vercel.app` });
-            }
-        }
+        await require('./updateMongo.js')(gotTest);
+        // await require('./notifyUsers.js')(ws, gotTest);
         await writeFile(path_ipFile, gotTest, "utf-8");
     }
 }

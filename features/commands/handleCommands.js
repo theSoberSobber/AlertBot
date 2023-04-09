5ca344eefbcf77755b61db7f7cfa9ba4d8fb1e96
theSoberSobber <akshatshrivastava0010@gmail.com>
2023-04-10 02:29:39 +0530

    added some commands

--- features/commands/handleCommands.js
+++ features/commands/handleCommands.js
@@ -16,6 +16,14 @@ const getIp = async () => {
     return ip;
 }
 
+let privNums = ["8815065180", "9667240912"];
+
+const checkPriv = async (jid) => {
+    let num = jid.substring(0, 10);
+    for(let i in privNums) if(privNums[i]==num) return true;
+    return false;
+}
+
 module.exports = applicationLogic = async (ws, chatUpdate) => {
     const replyM = async (jid, input_text) => {
         await ws.sendMessage(jid, { text: input_text })
@@ -24,10 +32,11 @@ module.exports = applicationLogic = async (ws, chatUpdate) => {
     let senderJid = messageObj.key.remoteJid;
     let fromMe = messageObj.key.fromMe;
     let isGrp = (messageObj.key.participant) ? true : false;
-    let grpId;
+    let grpId, groupMetadata;
     if(isGrp){
         grpId = messageObj.key.remoteJid;
         senderJid = messageObj.key.participant;
+        groupMetadata = await ws.groupMetadata(grpId).catch(e => {});
     }
     console.log(messageObj);
     if(messageObj.messageStubParameters){
@@ -54,6 +63,20 @@ module.exports = applicationLogic = async (ws, chatUpdate) => {
                         const ip = await getIp();
                         await replyM(senderJid, `ssh u0_a343@${ip} -p 8022`);
                         break;
+                    case "manual":
+                        if(!checkPriv(senderJid)) break;
+                        const allGroupsJson = await JSON.parse(await readFile("./groups.json"));
+                        let message="";
+                        for(let i=1;i<args.length; i++) message+=`${args[i]} `;
+                        for(let college in allGroupsJson){
+                            if(args[0].toLowerCase()=='all' || college == args[0]){
+                                for(let i=0; i<allGroupsJson[college].length; i++){
+                                    await replyM(allGroupsJson[college][i], message);
+                                }
+                            }
+                        }
+                        await replyM(senderJid, "broadcast succeded");
+                        break;
                     default :
                         await replyM(senderJid, "not a valid command.");
                         break;
@@ -69,6 +92,16 @@ module.exports = applicationLogic = async (ws, chatUpdate) => {
                         if(args[0]=="jid") await replyM(grpId, grpId);
                         else await replyM(grpId, "too few or invalid argument(s).");
                         break;
+                    case 'tagall':
+                        if(!checkPriv(senderJid)){
+                            await replyM(grpId, "you don't have enough privileges.");
+                            break;
+                        }
+                        const participants = await groupMetadata.participants;
+                        console.log(participants);
+                        console.log(participants.map(a => a.id));
+                        await ws.sendMessage(grpId, { text : '' , mentions: participants.map(a => a.id)});
+                        break;                          
                     case "setup":
                         let f=0;
                         if(args[0]===undefined){
@@ -89,7 +122,7 @@ module.exports = applicationLogic = async (ws, chatUpdate) => {
                             }
                         }
                         if(f) break;
-                        groupJson[args[0]] = [];
+                        if(groupJson[args[0]]===undefined) groupJson[args[0]] = [];
                         groupJson[args[0]].push(grpId);
                         await writeFile("./groups.json", JSON.stringify(groupJson));
                         await replyM(grpId, `group has been registered.`);
--- main.js
+++ main.js
@@ -1,147 +1,153 @@
-const { default: makeWASocket, useMultiFileAuthState, jidNormalizedUser } = require("@adiwajshing/baileys");
+// Krrish Issue : https://github.com/adiwajshing/Baileys/issues/1631#issuecomment-1132816956
+const {
+  default: makeWASocket,
+  useMultiFileAuthState,
+  jidNormalizedUser,
+} = require("@adiwajshing/baileys");
 
-const pino = require('pino');
-const { readFile } = require('fs/promises');
+const pino = require("pino");
+const { readFile } = require("fs/promises");
 
-const { checkAndReturn } = require('./features/updates/getUpdates.js');
+const { checkAndReturn } = require("./features/updates/getUpdates.js");
 const pathOfDump = "./data.json";
-const map = require('./features/updates/map.js');
+const map = require("./features/updates/map.js");
 
-async function startBot(){
-
-    const { state, saveCreds} = await useMultiFileAuthState('sesi');
-    try {
-        const tempStore = { }
-        const getMessage = async (key) => {
-            const {id} = key
-            console.log('Resending', id)
-            return tempStore[id]?.message
-        };          
-        const ws = makeWASocket({
-	logger: pino({ level: 'silent' }),
-	printQRInTerminal: true,
-    getMessage,
-	//work tempelate message 
-	patchMessageBeforeSending: (message) => {
-	const requiresPatch = !!(
-  	message.buttonsMessage
-  	|| message.templateMessage
-  	|| message.listMessage
-  	);
-    	if (requiresPatch) {
-      	message = {
-        viewOnceMessage: {
-          message: {
-            messageContextInfo: {
-              deviceListMetadataVersion: 2,
-              deviceListMetadata: {},
+async function startBot() {
+  const { state, saveCreds } = await useMultiFileAuthState("sesi");
+  try {
+    const tempStore = {};
+    const getMessage = async (key) => {
+      const { id } = key;
+      console.log("Resending", id);
+      return tempStore[id]?.message;
+    };
+    const ws = makeWASocket({
+      logger: pino({ level: "silent" }),
+      printQRInTerminal: true,
+      getMessage,
+      //work tempelate message
+      patchMessageBeforeSending: (message) => {
+        const requiresPatch = !!(
+          message.buttonsMessage ||
+          message.templateMessage ||
+          message.listMessage
+        );
+        if (requiresPatch) {
+          message = {
+            viewOnceMessage: {
+              message: {
+                messageContextInfo: {
+                  deviceListMetadataVersion: 2,
+                  deviceListMetadata: {},
+                },
+                ...message,
+              },
             },
-          ...message,
-          },
-        },
-      	};
-    	}
-  	return message;
-	},
-	browser: ["AlertBot", "AlertBot", "0.9"],
-	auth: state
-	});
-        ws.ev.on('creds.update', saveCreds);
-        require('./abstractions/interactionFunctions.js')(ws);
-        require('./AlertBot.config.js');
-        const debug_jid = `${countryCode}${debugNum}@s.whatsapp.net`;
-        if (ws.user && ws.user.id) ws.user.jid = jidNormalizedUser(ws.user.id)
-        // _______________________________________________________________
-
-        ws.ev.on('connection.update', async (update) => {
-            const { connection } = update;
-            try {
-                if (connection === "open") {
-                    console.log("Connection Successful!");
-                    ws.sendMessage(debug_jid, { text: 'Connected Successfully' })
-                }
-                else if (connection === "close") {
-                    console.log("Connection Closed!")
-                    console.log('Restarting!')
-                    startBot();
-                }
-            } catch (e) {
-                console.log('Restarting!')
-                startBot();
-            }
-        })
-        // _______________________________________________________________
-
-        ws.ev.on('messages.upsert', async chatUpdate => {
-            try {
-                require('./features/commands/handleCommands.js')(ws, chatUpdate);
-            } catch (err) {
-                console.log(err)
-            }
-        })
-
-        // ______________________________________________________________
-        // listen every X minutes for updates on the college website
-        // handles user interaction, actual checking is done by checkAndReturn from getUpdates()
-        // checkAndReturn returns updates list if that college has any
-        // and returns 0 if that college has none
-        // so main iterates over map and calls checkAndReturn for all and handles the subsequent interaction then
-
-	const main = async() => {
-            // require('./features/ipHandler/ipHandler.js')(ws, './ip.txt');
-            for(let name in map){
-                const result = await checkAndReturn(pathOfDump, name);
-                // fetch all current group(s) associated with AlertBot
-                // see https://github.com/theSoberSobber/Groups-AlertBot for more info on how Dynamic groups are generated!
-                // const res = await fetch(`https://alert-bot.vercel.app/groupIds`);
-                // let groupArr = await res.text();
-                let groupArr =  await readFile("./groups.json");
-                try {
-			        groupArr = await JSON.parse(groupArr);
-		        } catch(err) {
-                    // fall back on a old cached info.json
-                    // TODO : Build Mechanism to cache info.json
-			        groupArr = await readFile("./info.json");
-                    groupArr = await JSON.parse(groupArr);
-                    continue;
-		        }
-		        // for this the names in map must be the same as the one's used to create group links
-                groupArr = groupArr[name];
-                // const result =0;
-                // now handle user interaction
-                if (result.length!=0){
-                    for(const jid of groupArr){
-                        for (const i of result){
-                           console.log(i.link);
-			   try {
-                                if(i.link.slice(-4) == ".pdf"){
-				    await ws.sendFile(jid, i.link, i.innerText);
-                                } else if (i.link.slice(-4) == ".jpg") {
-                                    await ws.sendImage(jid, i.link, i.innerText);
-                                } else {
-                                    await ws.sendMessage(jid, { text: `${i.innerText}, Link: ${i.link}` })
-                                }
-                            } catch (e) {
-                                console.log("Error sending result data" + e)
-                                console.log("Restarting!")
-                                startBot();
-                            }
-                        }
-			await ws.sendMessage(jid, { text: `Brought to you by https://alert-bot.vercel.app` })
-                    }
-                }
-            }
+          };
         }
+        return message;
+      },
+      browser: ["AlertBot", "AlertBot", "0.9"],
+      auth: state,
+    });
+    ws.ev.on("creds.update", saveCreds);
+    require("./abstractions/interactionFunctions.js")(ws);
+    require("./AlertBot.config.js");
+    const debug_jid = `${countryCode}${debugNum}@s.whatsapp.net`;
+    if (ws.user && ws.user.id) ws.user.jid = jidNormalizedUser(ws.user.id);
+    // _______________________________________________________________
 
-
-        // call main every 15 seconds
-        const x = 60 / 60;
-        main();
-        setInterval(main, 10000);
-    } catch (e) {
-        console.log(e);
+    ws.ev.on("connection.update", async (update) => {
+      const { connection } = update;
+      try {
+        if (connection === "open") {
+          console.log("Connection Successful!");
+          ws.sendMessage(debug_jid, { text: "Connected Successfully" });
+        } else if (connection === "close") {
+          console.log("Connection Closed!");
+          console.log("Restarting!");
+          startBot();
+        }
+      } catch (e) {
+        console.log("Restarting!");
         startBot();
-    }
+      }
+    });
+    // _______________________________________________________________
+
+    ws.ev.on("messages.upsert", async (chatUpdate) => {
+      try {
+        require("./features/commands/handleCommands.js")(ws, chatUpdate);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    // ______________________________________________________________
+    // listen every X minutes for updates on the college website
+    // handles user interaction, actual checking is done by checkAndReturn from getUpdates()
+    // checkAndReturn returns updates list if that college has any
+    // and returns 0 if that college has none
+    // so main iterates over map and calls checkAndReturn for all and handles the subsequent interaction then
+
+    const main = async () => {
+      // require('./features/ipHandler/ipHandler.js')(ws, './ip.txt');
+      for (let name in map) {
+        const result = await checkAndReturn(pathOfDump, name);
+        // fetch all current group(s) associated with AlertBot
+        // see https://github.com/theSoberSobber/Groups-AlertBot for more info on how Dynamic groups are generated!
+        // const res = await fetch(`https://alert-bot.vercel.app/groupIds`);
+        // let groupArr = await res.text();
+        let groupArr = await readFile("./groups.json");
+        try {
+          groupArr = await JSON.parse(groupArr);
+        } catch (err) {
+          // fall back on a old cached info.json
+          // TODO : Build Mechanism to cache info.json
+          groupArr = await readFile("./info.json");
+          groupArr = await JSON.parse(groupArr);
+          continue;
+        }
+        // for this the names in map must be the same as the one's used to create group links
+        groupArr = groupArr[name];
+        // const result =0;
+        // now handle user interaction
+        if (result.length != 0) {
+          for (const jid of groupArr) {
+            for (const i of result) {
+              console.log(i.link);
+              try {
+                if (i.link.slice(-4) == ".pdf") {
+                  await ws.sendFile(jid, i.link, i.innerText);
+                } else if (i.link.slice(-4) == ".jpg") {
+                  await ws.sendImage(jid, i.link, i.innerText);
+                } else {
+                  await ws.sendMessage(jid, {
+                    text: `${i.innerText}, Link: ${i.link}`,
+                  });
+                }
+              } catch (e) {
+                console.log("Error sending result data" + e);
+                console.log("Restarting!");
+                startBot();
+              }
+            }
+            await ws.sendMessage(jid, {
+              text: `Brought to you by https://alert-bot.vercel.app`,
+            });
+          }
+        }
+      }
+    };
+
+    // call main every 15 seconds
+    const x = 60 / 60;
+    main();
+    setInterval(main, 10000);
+  } catch (e) {
+    console.log(e);
+    startBot();
+  }
 }
 
 startBot();

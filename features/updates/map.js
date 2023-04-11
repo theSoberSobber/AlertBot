5a87f43f618519b83f54309fdc5c90f9a78b11f9
theSoberSobber <akshatshrivastava0010@gmail.com>
2023-04-11 17:39:29 +0530

    added some parsers

--- features/updates/map.js
+++ features/updates/map.js
@@ -5,9 +5,11 @@
 
 module.exports = {
     // add your colleges name here
-    // IMPORTANT - please add the same name that you used while creating your AlertBot groups
-    // If you haven't got your group yet then visit `${base_uri}/[collegeName]/group`
+    // IMPORTANT - please add the same name that you used while registering your AlertBot groups
+    // If you haven't got your group yet then make a group, add the bot and run "/setup dtu"
     // write keys in lower case only, for example don't write MANIT instead of manit
-    "manit": require('./parsers/manitParser.js')
+    "manit": require('./parsers/manitParser.js'),
+    "iitg": require('./parsers/iitgParser.js'),
+    "iiitbh": require('./parsers/iiitbhParser.js')
     // "dtu": require('./parsers/dtuParser.js')
 }
---
+++ features/updates/parsers/iiitbhParser.js
@@ -0,0 +1,44 @@
+// all parsers are supposed to return list of objects where each object is an update of their college
+// where each object will have two attributes & "innerText" and "link"
+// ___________________________________
+// returns data[manit] list of updates
+const { load } = require('cheerio');
+const base = 'https://www.iiitbhopal.ac.in/media/notice';
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
+module.exports = async () => {
+    var list = [];
+
+    let res = await fetch('https://www.iiitbhopal.ac.in/api/CMSData/GetNoticeData1?Category=Notice');
+    res = await res.json();
+    // console.log(res);
+
+    for(let i in res){
+        // %4 pe 1,2,3 remainder vale concat karne for link
+        let link="";
+        for(let j in res[i]["NoticeData"].split(";")){
+            if(j%4==0){
+                link+=base;
+                continue;
+            }
+            if(j%4==3) link+=".";
+            else link+="/";
+            link+=res[i]["NoticeData"].split(";")[j].toLowerCase();
+            if(j%4==3){
+                list.push({
+                    innerText: res[i]["Title"],
+                    link: link,
+                });
+                link="";
+            }
+        }
+    }
+    return list;
+}
+
+if(require.main=== module){
+    (async ()=> {
+        console.log(await require("./iiitbhParser.js")());
+    })();
+}
